1360b672da9aa3aa0a33dfb2c91a39f38f912f57
Renmusxd <sumnernh@gmail.com>
2016-10-18 15:14:05 -0400

    Forgot to push perl

---
+++ static/bin/cms.pm
@@ -0,0 +1,24 @@
+package cms;
+
+my $netid = $ARGV[0];
+my $homeworkhandle = $ARGV[1];
+
+if (defined $homeworkhandle) {
+    open (HOMEWORK, $homeworkhandle);
+    open (SAVEDHW, ">", $netid);
+    foreach (<HOMEWORK>){
+        print SAVEDHW $_;
+    }
+    close(SAVEDHW);
+    close(HOMEWORK);
+    unlink $homeworkhandle;
+}
+
+if (open (NETID, $netid)){
+    open(LOGFILE, '>>', "hw_log.txt") or die "Error opening log";
+    print LOGFILE $netid;
+    close (NETID);
+    close (LOGFILE);
+}
+
+1;
